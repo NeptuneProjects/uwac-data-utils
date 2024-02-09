472acef3d9770256fceec497c048bc972b67d777
William Jenkins <wfjenkins@ucsd.edu>
2024-02-09 12:44:19 -0800

    blk fmt

--- src/datautils/read.py
+++ src/datautils/read.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from enum import Enum
 from pathlib import Path
-from typing import Optional, Protocol
+from typing import Optional
 
 import numpy as np
 
@@ -11,6 +10,18 @@ from datautils.formats.formats import FileFormat, validate_file_format
 from datautils.formats.shru import format_shru_headers, read_shru_headers
 from datautils.formats.sio import format_sio_headers, read_sio_headers
 from datautils.formats.wav import format_wav_headers, read_wav_headers
+
+
+def apply_header_formatting(
+    file_format: FileFormat, *args, **kwargs
+) -> tuple[list[Header], list[np.datetime64], list[np.datetime64]]:
+    """Apply time corrections to a list of records."""
+    if file_format == FileFormat.SHRU:
+        return format_shru_headers(*args, **kwargs)
+    if file_format == FileFormat.SIO:
+        return format_sio_headers(*args, **kwargs)
+    if file_format == FileFormat.WAV:
+        return format_wav_headers(*args, **kwargs)
 
 
 def get_headers_reader(
@@ -27,27 +38,6 @@ def get_headers_reader(
     raise ValueError(f"File format {file_format} is not recognized.")
 
 
-def read_headers(
-    filename: Path, file_format: str = None
-) -> tuple[list[Header], FileFormat]:
-    reader, file_format = get_headers_reader(
-        suffix=filename.suffix, file_format=file_format
-    )
-    return reader(filename), file_format
-
-
-def apply_header_formatting(
-    file_format: FileFormat, *args, **kwargs
-) -> tuple[list[Header], list[np.datetime64], list[np.datetime64]]:
-    """Apply time corrections to a list of records."""
-    if file_format == FileFormat.SHRU:
-        return format_shru_headers(*args, **kwargs)
-    if file_format == FileFormat.SIO:
-        return format_sio_headers(*args, **kwargs)
-    if file_format == FileFormat.WAV:
-        return format_wav_headers(*args, **kwargs)
-
-
 def get_sampling_rate(file_format: FileFormat, headers: list[Header]) -> float:
     """Get sampling rate from headers."""
     if file_format == FileFormat.SHRU:
@@ -56,3 +46,12 @@ def get_sampling_rate(file_format: FileFormat, headers: list[Header]) -> float:
         return headers[0].rhfs
     if file_format == FileFormat.WAV:
         return headers[0].framerate
+
+
+def read_headers(
+    filename: Path, file_format: str = None
+) -> tuple[list[Header], FileFormat]:
+    reader, file_format = get_headers_reader(
+        suffix=filename.suffix, file_format=file_format
+    )
+    return reader(filename), file_format
