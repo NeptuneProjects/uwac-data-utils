c08b91fa48c9f2093a6ead7314ee8a9d9d7ef74a
William Jenkins <wfjenkins@ucsd.edu>
2024-02-11 21:35:18 -0800

    Improve buffer performance; fix record indexing

--- src/datautils/data.py
+++ src/datautils/data.py
@@ -2,6 +2,7 @@
 
 from dataclasses import dataclass
 from enum import Enum
+import locale
 import logging
 from pathlib import Path
 from typing import Optional, Union
@@ -12,6 +13,14 @@ import polars as pl
 from datautils.catalogue import RecordCatalogue
 from datautils.query import CatalogueQuery
 from datautils.formats.shru import read_24bit_data
+
+locale.setlocale(locale.LC_ALL, "")
+
+MAX_BUFFER = int(2e9)
+
+
+class BufferExceededWarning(Warning):
+    pass
 
 
 class NoDataError(Exception):
@@ -70,6 +79,18 @@ class DataStream:
             self.time_end = self.time_init + np.timedelta64(
                 int(1e6 * self.num_samples / self.sampling_rate), "us"
             )
+
+    def __repr__(self) -> str:
+        """Returns string representation of the object."""
+        return (
+            f"DataStream(waveform={self.waveform}, "
+            f"channels={self.channels}, "
+            f"num_channels={self.num_channels}, "
+            f"num_samples={self.num_samples}, "
+            f"time_init={self.time_init}, "
+            f"time_end={self.time_end}, "
+            f"sampling_rate={self.sampling_rate})"
+        )
 
     # def load(self, filename: Path, exclude: Optional[str] = None) -> None:
     #     """Loads data from numpy file."""
@@ -133,59 +154,148 @@ class DataStream:
     #     return DataStream(X=self.X[index], t=self.t[index])
 
 
-def read(catalogue: RecordCatalogue, query: CatalogueQuery) -> DataStream:
-    """Reads data from catalogue using the query parameters."""
-    print(query.time_start, query.time_end)
+def read(query: CatalogueQuery, max_buffer: int = MAX_BUFFER) -> DataStream:
+    """Reads data from catalogue using the query parameters.
+
+    Args:
+        query (CatalogueQuery): Query parameters.
+        max_buffer (int): Maximum buffer length in samples.
+
+    Returns:
+        DataStream: Data stream object.
+
+    Raises:
+        NoDataError: If no data is found for the given query parameters.
+        ValueError: If multiple sampling rates are found in the catalogue.
+        BufferExceededWarning: If buffer length is less than expected samples.
+    """
+    catalogue = RecordCatalogue().load(query.catalogue)
+    num_channels = len(query.channels)
     df = select_records_by_time(catalogue.df, query.time_start, query.time_end)
+    with pl.Config(tbl_cols=df.width):
+        print(df)
 
     if len(df) == 0:
         raise NoDataError("No data found for the given query parameters.")
-    logging.info(f"Reading {len(df)} records.")
+    logging.debug(f"Reading {len(df)} records.")
 
     filenames = sorted(df.unique(subset=["filename"])["filename"].to_list())
     fixed_gains = df.unique(subset=["filename"])["fixed_gain"].to_list()
-    sampling_rates = df.unique(subset=["filename"])["sampling_rate"].to_list()
+    sampling_rates = (
+        df.unique(subset=["filename"])
+        .unique(subset=["sampling_rate"])["sampling_rate"]
+        .to_list()
+    )
+    if len(set(sampling_rates)) > 1:
+        raise ValueError(
+            "Multiple sampling rates found in the catalogue; unable to proceed."
+        )
+    sampling_rate = sampling_rates[0]
 
-    for filename, fixed_gain, sampling_rate in zip(
-        filenames, fixed_gains, sampling_rates
-    ):
-        records = df.filter(pl.col("filename") == filename)["record_number"].to_list()
+    report_buffer(max_buffer, num_channels, sampling_rate)
+    expected_samples = compute_expected_samples(df)
+    if expected_samples > max_buffer:
+        raise BufferExceededWarning(
+            f"Buffer length {max_buffer} is less than expected samples {expected_samples}."
+        )
 
+    logging.debug(f"Initializing buffer...")
+    waveform = -2009.0 * np.ones((expected_samples, num_channels))
+    logging.debug(f"Buffer initialized.")
+
+    marker = 0
+    for filename, fixed_gain in zip(filenames, fixed_gains):
+        rec_ind = df.filter(pl.col("filename") == filename)["record_number"].to_list()
+        logging.debug(f"Reading records {rec_ind} from {filename}.")
+
+        # TODO: Create reader factory to read different file formats
         data, header = read_24bit_data(
             filename=filename,
-            records=records,
+            records=rec_ind,
             channels=query.channels,
             fixed_gain=fixed_gain,
         )
-        ds = DataStream(
-            waveform=data,
-            channels=query.channels,
-            time_init=query.time_start,
-            time_end=query.time_end,
-            sampling_rate=sampling_rate,
-        )
-        print(ds.time_init, ds.time_end, ds.sampling_rate)
+        waveform[marker : marker + data.shape[0]] = data
+        marker += data.shape[0]
+
+        # if marker >= buffer:
+        #     break
 
         # TODO: Merge data and header into a single DataStream object
         # TODO: Enable time vector construction
-        # TODO: Create reader factory to read different file formats
+        # TODO: Trim the waveform to the specified time range
+        # TODO: Implement buffer to read data in chunks
 
-    return ds
+    if marker < max_buffer:
+        waveform = waveform[:marker]
+
+    return DataStream(
+        waveform=waveform,
+        channels=query.channels,
+        time_init=query.time_start,
+        time_end=query.time_end,
+        sampling_rate=sampling_rate,
+    )
 
 
 def select_records_by_time(
     df: pl.DataFrame, time_start: np.datetime64, time_end: np.datetime64
 ) -> pl.DataFrame:
     """Select files by time."""
+    logging.debug(f"Selecting records by time: {time_start} to {time_end}.")
     if time_start > time_end:
         raise ValueError("time_start must be less than time_end.")
     if np.isnat(time_start) and np.isnat(time_end):
         return df
     if time_start is not None and np.isnat(time_end):
-        return df.filter(pl.col("timestamp") >= time_start)
+        row_numbers = df.filter(pl.col("timestamp") >= time_start)["row_nr"].to_list()
+        row_numbers.insert(0, row_numbers[0] - 1)
+        mask = pl.col("row_nr").is_in(row_numbers)
+        return df.filter(mask)
     if np.isnat(time_start) and time_end is not None:
-        print("no time_start but time_end")
         return df.filter(pl.col("timestamp") <= time_end)
-    return df.filter(
+
+    row_numbers = df.filter(
         (pl.col("timestamp") >= time_start) & (pl.col("timestamp") <= time_end)
+    )["row_nr"].to_list()
+    row_numbers.insert(0, row_numbers[0] - 1)
+    mask = pl.col("row_nr").is_in(row_numbers)
+    return df.filter(mask)
+
+
+def report_buffer(buffer: int, num_channels: int, sampling_rate: float) -> None:
+    """Logs buffer size.
+
+    Args:
+        buffer (int): Buffer length in samples.
+        num_channels (int): Number of channels.
+        sampling_rate (float): Sampling rate.
+
+    Returns:
+        None
+    """
+    logging.debug(
+        f"MAX BUFFER ---- length: {buffer:e} samples | "
+        f"length per channel: {int(buffer / num_channels)} samples | "
+        f"size: {8 * buffer / 1e6:n} MB | "
+        f"duration: {buffer / sampling_rate / num_channels / 60:.3f} min with {num_channels} channels."
     )
+
+
+def compute_expected_samples(df: pl.DataFrame) -> int:
+    """Computes expected samples.
+
+    Args:
+        df (pl.DataFrame): Data frame.
+
+    Returns:
+        int: Expected samples.
+    """
+    expected_samples = 0
+    for filename in sorted(df.unique(subset=["filename"])["filename"].to_list()):
+        rec_ind = df.filter(pl.col("filename") == filename)["record_number"].to_list()
+        expected_samples += df.filter(
+            (pl.col("filename") == filename) & (pl.col("record_number").is_in(rec_ind))
+        )["npts"].sum()
+    logging.debug(f"Expected samples: {expected_samples}")
+    return expected_samples
