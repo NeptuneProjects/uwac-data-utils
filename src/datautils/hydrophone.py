b2a38075f9d6997f929d148d20e1dca5c8c626dd
William Jenkins <wfjenkins@ucsd.edu>
2024-02-12 19:27:58 -0800

    Add sensible defaults

--- src/datautils/hydrophone.py
+++ src/datautils/hydrophone.py
@@ -6,13 +6,29 @@ from typing import Optional
 
 @dataclass
 class Hydrophone:
+    """Hydrophone.
+
+    Args:
+        serial_number (int, optional): Serial number. Defaults to 0.
+        fixed_gain (float, optional): Fixed gain [dB]. Defaults to 0.0.
+        sensitivity (float, optional): Sensitivity [dB]. Defaults to 0.0.
+    """
+
     serial_number: int = 0
-    fixed_gain: float = 1.0
-    sensitivity: float = 1.0
+    fixed_gain: float = 0.0
+    sensitivity: float = 0.0
 
 
 @dataclass
 class HydrophoneSpecs:
-    fixed_gain: Optional[list[float]] = 1.0
-    sensitivity: Optional[list[float]] = 1.0
+    """Hydrophone specifications.
+
+    Args:
+        fixed_gain (Optional[list[float]], optional): Fixed gain [dB]. Defaults to 0.0.
+        sensitivity (Optional[list[float]], optional): Sensitivity [dB]. Defaults to 0.0.
+        serial_number (Optional[list[int]], optional): Serial number. Defaults to 0.
+    """
+
+    fixed_gain: Optional[list[float]] = 0.0
+    sensitivity: Optional[list[float]] = 0.0
     serial_number: Optional[list[int]] = 0
